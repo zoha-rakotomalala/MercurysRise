b0e172ee46de0db7af04eaf02fd74cf724ce9fc7
zoha-rakotomalala <zoha.rakotomalala@gmail.com>
2023-04-26 22:21:46 -0700

    fix for itch.io

--- Assets/Scripts/Character/Player.cs
+++ Assets/Scripts/Character/Player.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Burst.Intrinsics;
 using Unity.VisualScripting;
-using UnityEditor.Animations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
